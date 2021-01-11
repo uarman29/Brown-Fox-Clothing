c8f19142d8d7f5bb47d75cddc6a970a406beb2d5
Arman <uarman2215@gmail.com>
2021-01-11 18:52:11 -0500

    Moving directory data into its own section

--- src/components/directory/DirectoryComponent.js
+++ src/components/directory/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MenuItemComponent from '../menu-item/MenuItemComponent';
+import directory_data from '../../directory_data';
 import './DirectoryComponent.css';
 
 class DirectoryComponent extends React.Component
@@ -9,40 +10,7 @@ class DirectoryComponent extends React.Component
         super(props);
         this.state = 
         {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'shop/hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'shop/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shop/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shop/womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop/mens'
-                }
-            ]
+            sections: directory_data
         };
     }
 
