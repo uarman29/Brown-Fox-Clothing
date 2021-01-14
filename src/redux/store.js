a0f760136e07f494b50cc956f0d61c172cf444b3
Arman <uarman2215@gmail.com>
2021-01-14 18:49:19 -0500

    Fixing Title of page and removing logs

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,11 @@ import reducers from './reducers';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const store = createStore(reducers, applyMiddleware(logger, thunk));
+const middlewares = [thunk];
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+  }
+
+const store = createStore(reducers, applyMiddleware(...middlewares));
 
 export default store;
